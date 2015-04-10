0a1ad8e3909ce5a4ceac4aaed7ca7187e4b781bd
Derek Argueta <darguetap@gmail.com>
2015-04-09 23:14:58 -0600

    enabled html5Mode for pretty urls

--- public/js/script.js
+++ public/js/script.js
@@ -7,71 +7,71 @@
 /////////////////////////////
 $(document).ready(function() {
 
-      var pathname = window.location.href.split("#")[1];
-      if(pathname === "/work") {
-        $("#nav-indicator").removeClass("-1u");
-        $("#nav-indicator").addClass("-3u");
-      } else if(pathname === "/projects") {
-        $("#nav-indicator").removeClass("-1u");
-        $("#nav-indicator").addClass("-5u");
-      } else if(pathname === "/blog") {
-        $("#nav-indicator").removeClass("-1u");
-        $("#nav-indicator").addClass("-7u");
-      } else if(pathname === "/contact") {
-        $("#nav-indicator").removeClass("-1u");
-        $("#nav-indicator").addClass("-9u");
-      }
+  var pathname = window.location.href.split("/")[3];
+  if(pathname === "work") {
+    $("#nav-indicator").removeClass("-1u");
+    $("#nav-indicator").addClass("-3u");
+  } else if(pathname === "projects") {
+    $("#nav-indicator").removeClass("-1u");
+    $("#nav-indicator").addClass("-5u");
+  } else if(pathname === "blog") {
+    $("#nav-indicator").removeClass("-1u");
+    $("#nav-indicator").addClass("-7u");
+  } else if(pathname === "contact") {
+    $("#nav-indicator").removeClass("-1u");
+    $("#nav-indicator").addClass("-9u");
+  }
 
-      var clearNav = function() {
-        $("#nav-indicator").removeClass("-1u");
-        $("#nav-indicator").removeClass("-3u");
-        $("#nav-indicator").removeClass("-5u");
-        $("#nav-indicator").removeClass("-7u");
-        $("#nav-indicator").removeClass("-9u")
-      }
+  var clearNav = function() {
+    $("#nav-indicator").removeClass("-1u");
+    $("#nav-indicator").removeClass("-3u");
+    $("#nav-indicator").removeClass("-5u");
+    $("#nav-indicator").removeClass("-7u");
+    $("#nav-indicator").removeClass("-9u")
+  }
 
-      $("#home").click(function() {
-        if(!$("#nav-indicator").hasClass("-1u")) {
-          clearNav();
-          $("#nav-indicator").addClass("-1u");
-        }
-      });
+  $("#home").click(function() {
+    if(!$("#nav-indicator").hasClass("-1u")) {
+      clearNav();
+      $("#nav-indicator").addClass("-1u");
+    }
+  });
 
-      $("#work").click(function() {
-        if(!$("#nav-indicator").hasClass("-3u")) {
-          clearNav();
-          $("#nav-indicator").addClass("-3u");
-        }
-      });
+  $("#work").click(function() {
+    if(!$("#nav-indicator").hasClass("-3u")) {
+      clearNav();
+      $("#nav-indicator").addClass("-3u");
+    }
+  });
 
-      $("#projects").click(function() {
-        if(!$("#nav-indicator").hasClass("-5u")) {
-          clearNav();
-          $("#nav-indicator").addClass("-5u");
-        }
-      });
+  $("#projects").click(function() {
+    if(!$("#nav-indicator").hasClass("-5u")) {
+      clearNav();
+      $("#nav-indicator").addClass("-5u");
+    }
+  });
 
-      $("#blog").click(function() {
-        if(!$("#nav-indicator").hasClass("-7u")) {
-          clearNav();
-          $("#nav-indicator").addClass("-7u");
-        }
-      });
+  $("#blog").click(function() {
+    if(!$("#nav-indicator").hasClass("-7u")) {
+      clearNav();
+      $("#nav-indicator").addClass("-7u");
+    }
+  });
 
-      $("#contact").click(function() {
-        if(!$("#nav-indicator").hasClass("-9u")) {
-          clearNav();
-          $("#nav-indicator").addClass("-9u");
-        }
-      });
-    });
+  $("#contact").click(function() {
+    if(!$("#nav-indicator").hasClass("-9u")) {
+      clearNav();
+      $("#nav-indicator").addClass("-9u");
+    }
+  });
+});
 
 
 /////////////////////////////////////////
 //            AngularJS                //
 /////////////////////////////////////////
 var myApp = angular.module('myApp', ['ngRoute']);
-myApp.config(function($routeProvider) {
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
 
     .when('/', {
@@ -94,6 +94,10 @@ myApp.config(function($routeProvider) {
     .when('/contact', {
       templateUrl : '/pages/contact.html'
     });
-})
 
-myApp.controller('blogController', function($scope) {});
+    $locationProvider.html5Mode(true);
+}]);
+
+myApp.controller('blogController', function($scope) {
+
+});
