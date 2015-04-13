a58284952a7e5f2dab61e52ce00f29945d5d9c0c
Derek Argueta <darguetap@gmail.com>
2015-04-12 22:24:22 -0600

    Added bio

--- public/js/script.js
+++ public/js/script.js
@@ -9,59 +9,63 @@ $(document).ready(function() {
 
   var pathname = window.location.href.split("/")[3];
   if(pathname === "work") {
-    $("#nav-indicator").removeClass("-1u");
-    $("#nav-indicator").addClass("-3u");
+    $("#nav-indicator").addClass("-2u");
   } else if(pathname === "projects") {
-    $("#nav-indicator").removeClass("-1u");
-    $("#nav-indicator").addClass("-5u");
+    $("#nav-indicator").addClass("-4u");
   } else if(pathname === "blog") {
-    $("#nav-indicator").removeClass("-1u");
-    $("#nav-indicator").addClass("-7u");
+    $("#nav-indicator").addClass("-6u");
+  } else if(pathname === "bio") {
+    $("#nav-indicator").addClass("-8u");
   } else if(pathname === "contact") {
-    $("#nav-indicator").removeClass("-1u");
-    $("#nav-indicator").addClass("-9u");
+    $("#nav-indicator").addClass("-10u");
   }
 
   var clearNav = function() {
-    $("#nav-indicator").removeClass("-1u");
-    $("#nav-indicator").removeClass("-3u");
-    $("#nav-indicator").removeClass("-5u");
-    $("#nav-indicator").removeClass("-7u");
-    $("#nav-indicator").removeClass("-9u")
+    $("#nav-indicator").removeClass("-2u");
+    $("#nav-indicator").removeClass("-4u");
+    $("#nav-indicator").removeClass("-6u");
+    $("#nav-indicator").removeClass("-8u");
+    $("#nav-indicator").removeClass("-10u");
   }
 
   $("#home").click(function() {
     if(!$("#nav-indicator").hasClass("-1u")) {
       clearNav();
-      $("#nav-indicator").addClass("-1u");
     }
   });
 
   $("#work").click(function() {
-    if(!$("#nav-indicator").hasClass("-3u")) {
+    if(!$("#nav-indicator").hasClass("-2u")) {
       clearNav();
-      $("#nav-indicator").addClass("-3u");
+      $("#nav-indicator").addClass("-2u");
     }
   });
 
   $("#projects").click(function() {
-    if(!$("#nav-indicator").hasClass("-5u")) {
+    if(!$("#nav-indicator").hasClass("-4u")) {
       clearNav();
-      $("#nav-indicator").addClass("-5u");
+      $("#nav-indicator").addClass("-4u");
     }
   });
 
   $("#blog").click(function() {
-    if(!$("#nav-indicator").hasClass("-7u")) {
+    if(!$("#nav-indicator").hasClass("-6u")) {
       clearNav();
-      $("#nav-indicator").addClass("-7u");
+      $("#nav-indicator").addClass("-6u");
+    }
+  });
+
+  $("#bio").click(function() {
+    if(!$("#nav-indicator").hasClass("-8u")) {
+      clearNav();
+      $("#nav-indicator").addClass("-8u");
     }
   });
 
   $("#contact").click(function() {
-    if(!$("#nav-indicator").hasClass("-9u")) {
+    if(!$("#nav-indicator").hasClass("-10u")) {
       clearNav();
-      $("#nav-indicator").addClass("-9u");
+      $("#nav-indicator").addClass("-10u");
     }
   });
 });
@@ -93,6 +97,10 @@ myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $l
 
     .when('/contact', {
       templateUrl : '/pages/contact.html'
+    })
+
+    .when('/bio', {
+      templateUrl: '/pages/bio.html'
     });
 
     $locationProvider.html5Mode(true);
